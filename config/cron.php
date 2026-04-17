5aadc83a2609873c4867f54f39fdf74e98665589
Vivid-Marketing <hochoa@hsi.com>
2026-04-17 08:09:21 -0500

    cron fix

--- config/cron.php
+++ config/cron.php
@@ -1,22 +1,7 @@
 <?php
 
 return [
-    'token' => env('CRON_SCHEDULE_TOKEN'),
-];
-
-<?php
-
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cron token
-    |--------------------------------------------------------------------------
-    |
-    | Used by public cron entrypoints (Cloudways) to prevent unauthenticated
-    | execution. This value is included in config cache, so it will work even
-    | when `.env` is not loaded at runtime.
-    |
-    */
-    'token' => env('CRON_TOKEN', ''),
+    // Used by /internal/cron/schedule?token=... to run schedule:run.
+    'token' => env('CRON_SCHEDULE_TOKEN', ''),
 ];
 
